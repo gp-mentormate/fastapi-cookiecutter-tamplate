771966e4968931dbdff78cb7a19dc24779022a6f
Georgi Penev <georgi.penev@mentormate.com>
2023-06-19 17:20:26 +0300

    project structure refactoring

--- src/database.py
+++ src/database.py
@@ -1,32 +1,14 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-from .config import get_settings
+import databases
+from decouple import config
+from sqlalchemy.orm import declarative_base
+
+from .config import DatabaseSettings
 
 Base = declarative_base()
 
+settings = DatabaseSettings()
 
-class AsyncDatabaseSession:
-    def __init__(self):
-        self._session = None
-        self._engine = None
-
-    def __getattr__(self, name):
-        return getattr(self._session, name)
-
-    def init(self):
-        settings = get_settings()
-        self._engine = create_async_engine(
-            settings.db_config,
-            future=True,
-            echo=True,
-        )
-        self._session = sessionmaker(
-            self._engine, expire_on_commit=False, class_=AsyncSession
-        )()
-
-    async def create_all(self):
-        async with self._engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
-
-
-db = AsyncDatabaseSession()
+db = databases.Database(
+    settings.db_connection_url,
+    force_rollback=config("FASTAPI_TEST", cast=bool, default=False)
+)
