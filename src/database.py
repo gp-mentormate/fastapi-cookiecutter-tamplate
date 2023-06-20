ea7b2591d86c2281a9b62ef6c7b9da59df64ed8e
Georgi Penev <georgi.penev@mentormate.com>
2023-06-20 13:55:11 +0300

    update session name

--- src/database.py
+++ src/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, \
-    async_sessionmaker
+    async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
 
 from .config import DatabaseSettings
@@ -12,12 +12,12 @@ async_engine = create_async_engine(
     settings.db_connection_url, future=True
 )
 
-session = async_sessionmaker(async_engine)
+async_session = async_sessionmaker(async_engine)
 
 
-async def get_async_db():
-    db = session()
-    try:
-        yield db
-    finally:
-        await db.close()
+async def get_async_session() -> AsyncSession:
+    async with async_session() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
--- src/users/v1/crud.py
+++ src/users/v1/crud.py
@@ -3,7 +3,7 @@ from uuid import UUID
 from pydantic import EmailStr
 from sqlalchemy import select, insert
 
-from src.database import session
+from src.database import async_session
 from src.users.v1.models import User
 
 
@@ -12,7 +12,7 @@ class UserCRUD:
     # Create methods
     @staticmethod
     async def create_user(email: EmailStr, password: str):
-        async with session() as db:
+        async with async_session() as db:
             stmt = insert(User).values(email=email, password=password)
             result = await db.execute(stmt)
             await db.commit()
@@ -21,21 +21,21 @@ class UserCRUD:
     # Retrieve methods
     @staticmethod
     async def get_all_users():
-        async with session() as db:
+        async with async_session() as db:
             stmt = select(User)
             result = await db.execute(stmt)
             return result.scalars().all()
 
     @staticmethod
     async def get_user_by_id(user_id: UUID):
-        async with session() as db:
+        async with async_session() as db:
             statement = select(User).where(User.id == user_id)
             result = await db.execute(statement)
             return result.scalars().first()
 
     @staticmethod
     async def get_users_by_email(email: EmailStr):
-        async with session() as db:
+        async with async_session() as db:
             statement = select(User).where(User.email == email)
             result = await db.execute(statement)
             return result.scalars().all()
